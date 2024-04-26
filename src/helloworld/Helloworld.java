4b5a3e40e4a849638368ea1185a4917c72f9b17c
Kelton <kelton.tinho23@gmail.com>
2024-04-26 17:56:09 -0300

    first commit

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,18 @@
+package helloworld;
+
+
+
+
+
+public class Helloworld {
+	public static void ola() {
+		System.out.print("Ola");
+
+	}
+
+	public static void main(String[] args) {
+
+		ola();
+		
+	}
+}
